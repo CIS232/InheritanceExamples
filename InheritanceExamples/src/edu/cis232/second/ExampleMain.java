6f209d0d5212802b681a518f3bcc7b55650e1cb5
SK207_Inst <SK207_Inst@K207_Inst.carrollcc.edu>
2016-09-19 18:57:33 -0400

    Added Square

--- InheritanceExamples/src/edu/cis232/second/ExampleMain.java
+++ InheritanceExamples/src/edu/cis232/second/ExampleMain.java
@@ -4,9 +4,12 @@ public class ExampleMain {
 
 	public static void main(String[] args) {
 		Rectangle r = new Rectangle(2, 5);
+		System.out.println("Rectangle stats:");
 		System.out.println(r);
 		
-
+		Square s = new Square(5);
+		System.out.println("Square stats:");
+		System.out.println(s);
 	}
 
 }
